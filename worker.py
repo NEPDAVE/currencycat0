a21481c7a68c8ff691f49d95ebf86d55616fe3bb
NEPDAVE <nepdave1@gmail.com>
2016-09-01 15:22:26 -0700

    tried add the redis url as a configuration variable on the heroku dashboard

--- worker.py
+++ worker.py
@@ -1,13 +1,13 @@
 import os
 
-from redis import Redis
+import redis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-conn = Redis()
-#redis_url = 'localhost'
-#conn = redis.from_url(redis_url)
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+
+conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
