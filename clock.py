23d759bc5a2697dba6721887e13a54d688809dd8
NEPDAVE <nepdave1@gmail.com>
2016-09-01 07:34:48 -0700

    testing the clock

--- clock.py
+++ clock.py
@@ -3,9 +3,9 @@ from currencycat.database import seed_db
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', seconds=3)
+@sched.scheduled_job('interval', seconds=30)
 def timed_job():
-    #seed_db(pair='EUR_USD', count=1, granularity='M1')
+    seed_db(pair='EUR_USD', count=1, granularity='M1')
     print('This job is run every three seconds.')
 
 sched.start()
