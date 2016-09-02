2c8fe81632016a47da115635f0dc8cf0ae79cf45
NEPDAVE <nepdave1@gmail.com>
2016-09-01 18:50:25 -0700

    Installed redis to go and got I believe I got the correct redis configuration variables

--- worker.py
+++ worker.py
@@ -5,7 +5,9 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+#redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:e17dc9d6670e6c9aae6f3ce4ffcd03be@catfish.redistogo.com:11215/')
+
 
 conn = redis.from_url(redis_url)
 
